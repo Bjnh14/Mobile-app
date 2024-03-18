023dcdb61b8cc944dade8ec9edfcedd8182ca54d
Van Binh <vanbinhpham202@gmail.com>
2024-03-18 19:55:11 +0700

    lab1+2

---
+++ BinhPV/App.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import Hello from "./components/HelloComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <Hello name='BinhPV'/>
+    );
+  }
+}
+export default App
